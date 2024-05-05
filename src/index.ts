afbf83bb967b58a9a0d8ce87dd9cf3c77f95bbdd
Silas Berger <silas.berger@outlook.com>
2024-05-05 20:17:45 +0200

    Add some first demo routes.

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,13 @@ const HOSTNAME = "0.0.0.0";
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({message: "Hello, world!"})
+});
+
+app.get('/error', (req, res) => {
+  res.status(500);
+  res.send();
+});
+
 app.listen(PORT, HOSTNAME, () => console.log(`🚀 App listening on ${HOSTNAME}:${PORT}`));
