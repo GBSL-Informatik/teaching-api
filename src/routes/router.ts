7f830e9eae9c57eb57aca9f54d21e2ea6ee2ddb7
Silas Berger <silas.berger@outlook.com>
2024-07-05 12:05:43 +0200

    Add basic access for document roots.

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,19 +1,15 @@
-import express, { RequestHandler } from 'express';
+import express from 'express';
 import { all as allUsers, user, find as findUser, update as updateUser } from '../controllers/users';
 import {
-    all as allGroups,
-    find as findGroup,
-    update as updateGroup,
-    create as createGroup,
-    destroy as deleteGroup
-} from '../controllers/groups';
-import {
-    all as allDocuments,
-    find as findDocument,
-    update as updateDocument,
-    create as createDocument,
-    destroy as deleteDocument
+  all as allDocuments,
+  find as findDocument,
+  update as updateDocument,
+  create as createDocument,
+  destroy as deleteDocument
 } from '../controllers/documents';
+import {
+  find as findDocumentRoot
+} from '../controllers/document-roots';
 
 // initialize router
 const router = express.Router();
@@ -24,11 +20,13 @@ router.get('/users', allUsers);
 router.get('/users/:id', findUser);
 router.put('/users/:id', updateUser);
 
-router.get('/groups', allGroups);
-router.post('/groups', createGroup);
-router.get('/groups/:id', findGroup);
-router.put('/groups/:id', updateGroup);
-router.delete('/groups/:id', deleteGroup);
+// router.get('/groups', allGroups);
+// router.post('/groups', createGroup);
+// router.get('/groups/:id', findGroup);
+// router.put('/groups/:id', updateGroup);
+// router.delete('/groups/:id', deleteGroup);
+
+router.get('/documentRoots/:id', findDocumentRoot);
 
 router.get('/documents', allDocuments);
 router.post('/documents', createDocument);
