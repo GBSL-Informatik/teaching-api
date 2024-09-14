fcc840c82cb8321398345d518af847c608e63dc7
Silas Berger <silas.berger@outlook.com>
2024-09-14 20:25:00 +0200

    Run formatter.

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,7 +2,10 @@ import Logger from '../utils/logger';
 import { getStrategy as mockStrategy } from './mock';
 import { getStrategy } from './azureAD';
 export const strategyForEnvironment = () => {
-    if (process.env.NODE_ENV === 'test' || (process.env.NO_AUTH === 'true' && process.env.NODE_ENV !== 'production')) {
+    if (
+        process.env.NODE_ENV === 'test' ||
+        (process.env.NO_AUTH === 'true' && process.env.NODE_ENV !== 'production')
+    ) {
         if (process.env.NO_AUTH) {
             if (process.env.NODE_ENV !== 'test') {
                 Logger.info(
