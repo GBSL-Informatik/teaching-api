f46e8b747c39013d3db44a8d2c227fa9ce32a501
bh0fer <lebalz@outlook.com>
2025-06-04 17:45:01 +0000

    remove redundant name from ai template

---
+++ prisma/migrations/20250604174413_remove_name_from_ai_template/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `ai_templates` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ai_templates" DROP COLUMN "name";
