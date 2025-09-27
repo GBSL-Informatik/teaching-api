cbc4a2e621e8d66a483946602a8d8a8c67fcada5
bh0fer <lebalz@outlook.com>
2025-09-27 12:24:46 +0000

    config

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,39 @@
-import { PrismaClient } from '@prisma/client';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import prisma from './prisma';
+import { Role } from '@prisma/client';
 // If your Prisma file is located elsewhere, you can change the path
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: 'postgresql' // or "mysql", "sqlite", ...etc
-    })
+        provider: 'postgresql',
+        usePlural: true
+    }),
+    user: {
+        modelName: 'users',
+        additionalFields: {
+            role: {
+                type: 'string',
+                required: false,
+                defaultValue: Role.STUDENT,
+                input: false // don't allow user to set role
+            },
+            firstName: {
+                type: 'string',
+                required: false,
+                input: false
+            },
+            lastName: {
+                type: 'string',
+                required: false,
+                input: false
+            }
+        }
+    },
+    session: {
+        modelName: 'sessions'
+    },
+    account: {
+        modelName: 'accounts'
+    }
 });
