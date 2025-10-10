4df442af081e54335536f5c4852774ea3fc65ce2
bh0fer <lebalz@outlook.com>
2025-10-10 16:44:20 +0000

    proxy

--- src/auth.ts
+++ src/auth.ts
@@ -33,7 +33,7 @@ export const auth = betterAuth({
             tenantId: process.env.MSAL_TENANT_ID || 'common', // Use 'common' for multi-tenant applications
             authority: 'https://login.microsoftonline.com', // Authentication authority URL
             prompt: 'select_account', // Forces account selection,
-            // redirectURI: `${process.env.BETTER_AUTH_URL}/api/auth/callback/microsoft`,
+            redirectURI: `${process.env.BETTER_AUTH_URL}/api/auth/callback/microsoft`,
             mapProfileToUser: (profile) => {
                 const email = (profile.email || profile.preferred_username)?.toLowerCase();
                 const name = getNameFromMsftProfile(profile);
@@ -62,8 +62,8 @@ export const auth = betterAuth({
     plugins: [
         oneTimeToken(),
         admin({ defaultRole: 'student', adminRoles: ['teacher', 'admin'] }),
-        sso()
-        // oAuthProxy()
+        sso(),
+        oAuthProxy()
     ],
     logger: {
         level: 'info'
