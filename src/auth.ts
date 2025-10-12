5d1029a8de7dc50d17e7feaf5183d225a6f1d346
bh0fer <lebalz@outlook.com>
2025-10-12 10:32:13 +0000

    fix cb

--- src/auth.ts
+++ src/auth.ts
@@ -34,7 +34,7 @@ export const auth = betterAuth({
             authority: 'https://login.microsoftonline.com', // Authentication authority URL
             prompt: 'select_account', // Forces account selection,
             responseMode: 'query',
-            redirectURI: process.env.BETTER_AUTH_URL,
+            redirectURI: `${process.env.BETTER_AUTH_URL}/api/auth/callback/microsoft`,
             mapProfileToUser: (profile) => {
                 const email = (profile.email || profile.preferred_username)?.toLowerCase();
                 const name = getNameFromMsftProfile(profile);
