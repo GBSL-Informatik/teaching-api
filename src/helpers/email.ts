6d948533187960cb72b22b1382680e776128e306
bh0fer <lebalz@outlook.com>
2026-07-09 08:32:58 +0000

    upgrade better auth

--- src/helpers/email.ts
+++ src/helpers/email.ts
@@ -1,6 +1,6 @@
 import _ from 'es-toolkit/compat';
 
-export const getNameFromEmail = (email?: string) => {
+export const getNameFromEmail = (email?: string | null) => {
     const name = (email || '').split('@')[0] || '';
     const firstName = _.capitalize(name.split('.')[0] || '');
     const lastName = _.capitalize(name.split('.')[1] || '');
